f416348cdf42e18384e120eb4d721b9baf8ffb68
Ritesh Chauhan <chauhanx.ritesh@gmail.com>
2024-03-07 17:41:06 +0530

    pipenv

--- ice_breaker.py
+++ ice_breaker.py
@@ -14,13 +14,22 @@ def ice_breaker(information: str):
     1. A short summary of the person's life and achievements.
     2. Two interesting facts about the person.
     """
-
+    api_key = os.getenv('AZURE_OPENAI_API_KEY')
+    deployment_name = os.getenv('DEPLOYMENT_NAME')
+    azure_endpoint = os.getenv('AZURE_ENDPOINT')
+    
+    # print(f"API Key: {api_key}")
+    # print(f"Deployment Name: {deployment_name}")
+    # print(f"Azure Endpoint: {azure_endpoint}")
+    
     prompt_template = PromptTemplate(input_variables=['information'], template=summary_template)
     chat_model = AzureChatOpenAI(
-        api_key=os.getenv('AZURE_OPENAI_API_KEY'),
-        deployment_name=os.getenv('AZURE_DEPLOYMENT_NAME'),
+        api_key=api_key,
+        deployment_name=deployment_name,
+        azure_endpoint=azure_endpoint,
         openai_api_version="2024-02-15-preview",
     )
+
     name = "Eden Marco"
     linkedin_url = lookup(name=name, llm=chat_model)
     print(f"Linkedin URL for {name} is: {linkedin_url}")
